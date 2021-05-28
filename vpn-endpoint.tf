20f0b3f866c59cec6035e93b2922f3e0bda5d083
Stuart MacDonald <32204841+labkey-stuartm@users.noreply.github.com>
2021-05-28 14:18:33 -0700

    add default route to allow outbound internet access

--- vpn-endpoint.tf
+++ vpn-endpoint.tf
@@ -3,6 +3,7 @@ resource "aws_ec2_client_vpn_endpoint" "default" {
   server_certificate_arn = aws_acm_certificate.server.arn
   client_cidr_block      = var.cidr
   split_tunnel           = var.split_tunnel
+  dns_servers            = var.dns_servers
 
   authentication_options {
     type                       = "certificate-authentication"
@@ -18,10 +19,11 @@ resource "aws_ec2_client_vpn_endpoint" "default" {
   tags = var.tags
 }
 
+# use only first subnet to reduce monthly cost of vpn
 resource "aws_ec2_client_vpn_network_association" "default" {
-  count                  = length(var.subnet_ids)
+  #count                  = length(var.subnet_ids)
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.default.id
-  subnet_id              = element(var.subnet_ids, count.index)
+  subnet_id              = element(var.subnet_ids, 0)
 }
 
 resource "aws_ec2_client_vpn_authorization_rule" "default" {
@@ -29,4 +31,13 @@ resource "aws_ec2_client_vpn_authorization_rule" "default" {
   target_network_cidr    = "0.0.0.0/0"
   authorize_all_groups   = true
   description            = var.name
+}
+
+# Add route to allow outbound access
+resource "aws_ec2_client_vpn_route" "default" {
+  #count                  = length(var.subnet_ids)
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.default.id
+  destination_cidr_block = "0.0.0.0/0"
+  target_vpc_subnet_id   = element(var.subnet_ids, 0)
+  depends_on             = [aws_ec2_client_vpn_network_association.default]
 }
